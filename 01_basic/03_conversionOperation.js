123fa06391667e96bf3b11d5edd0f19abfb652c7
Pankaj Negi <97275626+Pankaj-Negi02@users.noreply.github.com>
2023-12-29 09:57:40 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,5 +23,37 @@ let booleanLoggedIn = Boolean(isLoggedIn);
 //String Coversion
 let colorName = 123
 let stringCovert = String(colorName);
-console.log(stringCovert);
-console.log(typeof stringCovert);
+// console.log(stringCovert);
+// console.log(typeof stringCovert);
+
+// *************** Operations *******************
+
+let value = 10
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(5-2);
+// console.log(2*2)
+// console.log(2**2)
+// console.log(10/2)
+// console.log(2%22)
+
+let str1 = "Hi"
+let str2 = " Pankaj Negi"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2 );
+// console.log(1 + "2" );
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2 + 4);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let counter = 10
+counter++
+console.log(counter);
