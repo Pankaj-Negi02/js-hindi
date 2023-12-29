d656450a9d8acd0b3d13bed19fb52f29bc2790d0
Pankaj Negi <97275626+Pankaj-Negi02@users.noreply.github.com>
2023-12-29 06:45:25 +0000

    Datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all JS code as news version
+
+// Datatypes
+// 1- string => ""
+// 2- number => 2 to power 53
+// 3- bigint
+// 4- boolean => true/false
+// 5- null => standalone value
+// 6- undefined =>
+// 7- symbol =>
+
+// 8- object
+
+let name = "Pankaj" // String
+let age = 25 // Number
+let isLoggedIn = true // Boolean
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
