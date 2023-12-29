5a1f71b386d7d56c94d8620d0dd5806fad84fed5
Pankaj Negi <97275626+Pankaj-Negi02@users.noreply.github.com>
2023-12-29 06:14:57 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountEmail = "pankaj@gmail.com"
+var accountPassword = "123456"
+accountCity = "Noida"
+let accountState;
+
+//accountId = 10 // Not allowed
+accountEmail = "negi@test.com"
+accountPassword = "101010"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
