49f250a8139a7bdf6c6d33f4e87c8319d3435d8d
Pankaj Negi <97275626+Pankaj-Negi02@users.noreply.github.com>
2023-12-29 07:59:51 +0000

    Conversion of Datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//Number Conversion
+let valueInNumber = Number(score);
+//console.log(valueInNumber);
+
+// "44" => 44
+// "44xyz" => NaN
+// true => 1; false => 0;
+
+// Boolean Conversion
+let isLoggedIn = "Pankaj"
+let booleanLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanLoggedIn);
+
+// 1 => true
+// 0 => false
+// "Pankaj" => true
+// "" => false
+
+//String Coversion
+let colorName = 123
+let stringCovert = String(colorName);
+console.log(stringCovert);
+console.log(typeof stringCovert);
